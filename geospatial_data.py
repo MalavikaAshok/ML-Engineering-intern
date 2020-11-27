9aa39fdebd84ff8506333fd8532f19a2119b2202
MalavikaAshok <44550883+MalavikaAshok@users.noreply.github.com>
2020-11-27 16:24:52 +0530

    Add files via upload

---
+++ geospatial_data.py
@@ -0,0 +1,84 @@
+import cdsapi
+
+c = cdsapi.Client()
+
+c.retrieve(
+    'reanalysis-era5-single-levels',
+    {
+        'product_type': 'reanalysis',
+        'format': 'netcdf',
+        'variable': [
+            '2m_temperature', 'total_precipitation', 'volumetric_soil_water_layer_1',
+        ],
+        'year': '2019',
+        'month': '12',
+        'day': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07', '08', '09',
+            '10', '11', '12',
+            '13', '14', '15',
+            '16', '17', '18',
+            '19', '20', '21',
+            '22', '23', '24',
+            '25', '26', '27',
+            '28', '29', '30',
+            '31',
+        ],
+        'time': '12:00',
+    },
+    'temperature.nc')
+
+
+c.retrieve(
+    'reanalysis-era5-single-levels',
+    {
+        'product_type': 'reanalysis',
+        'format': 'netcdf',
+        'variable': 'total_precipitation',
+        'year': '2019',
+        'month': '12',
+        'day': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07', '08', '09',
+            '10', '11', '12',
+            '13', '14', '15',
+            '16', '17', '18',
+            '19', '20', '21',
+            '22', '23', '24',
+            '25', '26', '27',
+            '28', '29', '30',
+            '31',
+        ],
+        'time': '12:00',
+    },
+    'precipitation.nc')
+
+
+c.retrieve(
+    'reanalysis-era5-single-levels',
+    {
+        'product_type': 'reanalysis',
+        'format': 'netcdf',
+        'variable': 'volumetric_soil_water_layer_1',
+        'year': '2019',
+        'month': '12',
+        'day': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07', '08', '09',
+            '10', '11', '12',
+            '13', '14', '15',
+            '16', '17', '18',
+            '19', '20', '21',
+            '22', '23', '24',
+            '25', '26', '27',
+            '28', '29', '30',
+            '31',
+        ],
+        'time': '12:00',
+    },
+    'soil.nc')
+
+
